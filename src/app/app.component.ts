2dcd140ab7fc6059a5f3d85630a87e0202e649db
Evan Schultz <evan@rangle.io>
2016-10-06 12:03:55 -0400

    AoT expirements
    * `@select` doesn't seem to work - but wont throw errors
    * can use .select().subscribe - no problem
    * can use this.x$ = this.ngRedux.select(...) fine and | async in the
    * template ok
    * is there anything I need to do for metadata generation for the select
    * decorator?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgRedux, select } from 'ng2-redux';
+import { exportedSelector } from './shared';
+import { Observable } from 'rxjs';
+export const selectTitleSelector = n => {
+  console.log('selectTitleSelector called?');
+  return n.test.selectTitle;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  subscribeTitle: string = 'not yet';
+  @select(selectTitleSelector) selectTitle$: Observable<string>;
+  @select(['test', 'keyPath']) keyPath$: Observable<string>;
+  @select(exportedSelector) exported$:  Observable<string>;
+  selectButNotSub$: Observable<string>;// = new Observable<string>();
+  constructor(private ngRedux: NgRedux<any>) {
+
+  }
+  ngOnInit() {
+    this.selectButNotSub$ = this.ngRedux.select(n => {
+       return n.test.selectButNotSub;
+      });
+    this.ngRedux.select(n => n.test).subscribe(n => {
+      console.log('n is', n);
+      this.subscribeTitle = n.subscribeTitle;
+    });
+  }
 }
